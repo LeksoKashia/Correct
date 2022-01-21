60378d8785870e2a4bd2d19a5a002af341265042
LeksoKashia <lekso.kashia.1@btu.edu.ge>
2022-01-21 22:09:15 +0400

    almost done,it just need a little shaping

--- app/src/main/java/com/example/correct/InsideAppFragments/CartFragment.kt
+++ app/src/main/java/com/example/correct/InsideAppFragments/CartFragment.kt
@@ -30,7 +30,7 @@ class CartFragment:Fragment(R.layout.cart_fragment) {
                 "DPI: 6500",
                 "Brand: Razer",
                 "Movement: optical",
-                "Conectivity: USB"
+                "Conectivity: Optical"
 
             )
 
@@ -39,13 +39,13 @@ class CartFragment:Fragment(R.layout.cart_fragment) {
         mouseList.add(
             Mouse(
                 2,
-                "https://onlinegaminggear.com/wp-content/uploads/2017/11/design-for-game.jpg",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKl9t4jaONDoqoK19p-ALr6sLeZWSbW9esurP1L7P5Po9v1Mn-W-Ob11RWnL1iDqvTkfI&usqp=CAU",
                 "Price: $23.50",
-                "Razer DeathAdder",
-                "DPI: 6500",
-                "Brand: Razer",
+                "Ironclaw RGB",
+                "DPI: 18,000",
+                "Brand: Corsair",
                 "Movement: optical",
-                "Conectivity: USB"
+                "Conectivity: Wireless, Wired"
 
             )
 
@@ -54,13 +54,13 @@ class CartFragment:Fragment(R.layout.cart_fragment) {
         mouseList.add(
             Mouse(
                 3,
-                "https://onlinegaminggear.com/wp-content/uploads/2017/11/design-for-game.jpg",
-                "Price: $23.50",
-                "Razer DeathAdder",
-                "DPI: 6500",
-                "Brand: Razer",
+                "https://www.windowscentral.com/sites/wpcentral.com/files/styles/large_wm_brw/public/field/image/2017/08/steelseries-rival-310-4.jpg?itok=JKzqN3Xx",
+                "Price: $69.99",
+                "rival 310",
+                "DPI: 12,000",
+                "Brand: steelseries",
                 "Movement: optical",
-                "Conectivity: Optical"
+                "Conectivity: Wireless"
 
             )
 
@@ -69,13 +69,13 @@ class CartFragment:Fragment(R.layout.cart_fragment) {
         mouseList.add(
             Mouse(
                 4,
-                "https://onlinegaminggear.com/wp-content/uploads/2017/11/design-for-game.jpg",
-                "Price: $23.50",
-                "Razer DeathAdder",
-                "DPI: 6500",
-                "Brand: Razer",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK9f0IRY2i3CCKuANC8LW0CgcSBy9LIe0lzQ&usqp=CAU",
+                "Price: $34.99 ",
+                " LOGITECH G305 LIGHTSPEED",
+                "DPI: 12,000",
+                "Brand: LOGITECH",
                 "Movement: optical",
-                "Conectivity: Optical"
+                "Conectivity: Wireless"
 
             )
 
@@ -84,13 +84,13 @@ class CartFragment:Fragment(R.layout.cart_fragment) {
         mouseList.add(
             Mouse(
                 5,
-                "https://onlinegaminggear.com/wp-content/uploads/2017/11/design-for-game.jpg",
-                "Price: $23.50",
-                "Razer DeathAdder",
-                "DPI: 6500",
+                "https://m.media-amazon.com/images/I/51n+5srvaUL.jpg",
+                "Price: $49.99",
+                "Razer Basilik Essential",
+                "DPI: 18,000",
                 "Brand: Razer",
                 "Movement: optical",
-                "Conectivity: Optical"
+                "Conectivity: Wired"
 
             )
 
@@ -99,13 +99,13 @@ class CartFragment:Fragment(R.layout.cart_fragment) {
         mouseList.add(
             Mouse(
                 6,
-                "https://onlinegaminggear.com/wp-content/uploads/2017/11/design-for-game.jpg",
-                "Price: $23.50",
-                "Razer DeathAdder",
-                "DPI: 6500",
-                "Brand: Razer",
+                "https://static.wixstatic.com/media/f1db3a_cf0b8759f27c43f7a5e0d2e27c8e65e6~mv2.jpg/v1/fill/w_4240,h_2400,al_c,q_85/f1db3a_cf0b8759f27c43f7a5e0d2e27c8e65e6~mv2.jpg",
+                "Price: $49.99",
+                "HyperX Pulsefire Haste",
+                "DPI: 3,200",
+                "Brand: HyperX",
                 "Movement: optical",
-                "Conectivity: Optical"
+                "Conectivity: Wired"
 
             )
 
@@ -114,22 +114,18 @@ class CartFragment:Fragment(R.layout.cart_fragment) {
         mouseList.add(
             Mouse(
                 7,
-                "https://onlinegaminggear.com/wp-content/uploads/2017/11/design-for-game.jpg",
-                "Price: $23.50",
-                "Razer DeathAdder",
+                "https://sm.ign.com/ign_me/screenshot/default/fk-4_mrqe.jpg",
+                "Price: $114.00",
+                "Zowie FK series",
                 "DPI: 6500",
-                "Brand: Razer",
+                "Brand: Zowie",
                 "Movement: optical",
-                "Conectivity: Optical"
+                "Conectivity: Wired"
 
             )
 
 
         )
-
-
-
-
 
 
 
